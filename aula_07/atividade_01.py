092554e54d3aa65df5039f8a4767e141fb53a034
ANALISTA DE DADOS - BIG DATA SCIENCE 2025.1 <gabriel.harduim.ti@gmail.com>
2025-04-29 20:47:35 -0300

    inicio primeira tividade

---
+++ aula_07/atividade_01.py
@@ -0,0 +1,5 @@
+#Um programa que, dado o, número calcule o dobro, o triplo e o quadrado desse número para agilizar seus orçamentos
+    #O dobro da quantidade solicitada (para ofertas promocionais)
+    #O triplo (para grandes eventos)
+    #E a area total de um adesivo considerando  que o número informado pe o lado do adesivo em centímetros 
+#Crie uma função que faça esses calculos e apresente na tela os resultados
