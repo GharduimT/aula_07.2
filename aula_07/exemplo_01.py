1aeaca42dddf891fe42c95d62d6f5ac03ca8358e
ANALISTA DE DADOS - BIG DATA SCIENCE 2025.1 <gabriel.harduim.ti@gmail.com>
2025-04-29 20:04:56 -0300

    primeiro uso def

---
+++ aula_07/exemplo_01.py
@@ -0,0 +1,21 @@
+#Funções em python inicia com a palavra
+#reservada def.
+#Funções são rotinas em seu conceito
+# Um algorítmo é composte de várias funções associadas
+#rotina - fazem sentido pra algo que eu vou ter qque fazer toda hora (tirar media ex)
+
+
+
+
+
+# def mostrar_linha():
+#     print(30*"=") # VAI ESCREVER 30X O QUESTÁ DENTRO DA STRING # 
+
+# mostrar_linha()
+# print('MODULO 01')
+# mostrar_linha()
+# print('ALGORÍTMOS')
+# mostrar_linha()
+# print('ANÁLISE DE DADOS')
+
+
