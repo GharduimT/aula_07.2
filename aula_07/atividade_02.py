42213f59f75e499e37453b19654c4664a6dbdb35
ANALISTA DE DADOS - BIG DATA SCIENCE 2025.1 <gabriel.harduim.ti@gmail.com>
2025-04-29 21:33:30 -0300

    a

---
+++ aula_07/atividade_02.py
@@ -0,0 +1,12 @@
+# pescador só pode pescar até 100 kg sem pagar multa. Passando de 100 kg deverá pagar multa de 4 reais por kg
+#logo o sistema só vai calcular o valor da multa se  antes o sistema identificar que o peso excedeu o limite
+
+
+# limite = 100
+
+# multa = 4
+
+peso = int(input('Informe o peso: '))
+
+if peso > 100:
+    multa = peso *0,4
