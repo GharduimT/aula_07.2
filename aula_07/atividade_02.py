fbe5834494bce74b03c4fc12918ddb5b0b818886
GharduimT <gabriel.harduim@gmail.com>
2025-05-01 22:32:42 -0300

    codificado com explicações

--- aula_07/atividade_02.py
+++ aula_07/atividade_02.py
@@ -9,3 +9,26 @@
 #     multa = peso *0,4
 
 '''Tentativa de resolver o problema em casa'''
+#limite=100
+#excesso=qualquer valor acima de 100
+    # nesse caso informar o valor, calculado por quilo, da multa 
+    # do contrário informar que não foi ultrapasado o peso limite
+
+peso = float(input(f'Informe o peso da pescado em quilos: '))
+'''abaixo: se peso for maior que xyz, (saiba que excesso é = peso - 100 e multa é = excesso *4) imprima... se não imprima...'''
+if peso > 100:
+    excesso = peso - 100
+    multa = excesso * 4
+    print(f'peso máximo excedido, sua multa será de: {multa:.2f}') #''' chvs multa:.2f chvs explicação no fim'''
+else:
+    print('peso limite NÃO excedido, NÃO há multa')
+
+''' {multa}: Insere o valor da variável multa na string
+
+    :.2f: Especifica como formatar o número:
+
+        : → Indica que vem uma especificação de formato
+
+        .2 → Define 2 casas decimais
+
+        f → Indica que é um número float (ponto flutuante)'''
